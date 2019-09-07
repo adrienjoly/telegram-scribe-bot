cde90afeba8e050cf04720d8b7d8a5c7a5c8ed06
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2019-09-07 14:38:19 +0200

    refacto: extract Ticktick class, and call it from TS file

---
+++ functions/src/Ticktick.ts
@@ -0,0 +1,29 @@
+import * as ticktick from 'ticktick-wrapper'
+
+export class Ticktick {
+  emailCreds: { username: string; password: string }
+
+  constructor(email: string, password: string) {
+    this.emailCreds = { username: email, password }
+  }
+
+  async connect() {
+    try {
+      await ticktick.login({ email: this.emailCreds })
+    } catch (err) {
+      throw new Error(
+        `Error while trying to login to ticktick.com: ${err.stack}`
+      )
+    }
+  }
+
+  async addTask(title: string, desc?: string) {
+    try {
+      await ticktick.Inbox.addSimpleTask(title, desc)
+    } catch (err) {
+      throw new Error(
+        `Error while trying to add a task to ticktick.com: ${err.stack}`
+      )
+    }
+  }
+}
---
+++ functions/tools/ticktick.ts
@@ -0,0 +1,29 @@
+require('dotenv').config({ path: `${__dirname}/../../.env` }) // load environment variables
+import { Ticktick } from './../src/Ticktick'
+
+const { TICKTICK_EMAIL, TICKTICK_PASSWORD } = process.env
+
+console.log(`${__dirname}/../../.env`)
+
+const main = async () => {
+  const ticktick = new Ticktick(
+    TICKTICK_EMAIL as string,
+    TICKTICK_PASSWORD as string
+  )
+  await ticktick.connect()
+  const task = {
+    title: 'Automated task',
+    desc: 'This is a task added programatically via ticktick-wrapper',
+  }
+  await ticktick.addTask(task.title, task.desc)
+  return task
+}
+
+main()
+  .then(task => {
+    console.log('✅  Successfully added task:', task)
+  })
+  .catch(err => {
+    console.error(err)
+    process.exit(1)
+  })
