6905b8d272fca37b94097df7ba5040cc830d8571
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2019-12-15 22:42:26 +0100

    prettier pass

--- functions/src/messageHandler.ts
+++ functions/src/messageHandler.ts
@@ -1,4 +1,8 @@
-import { TelegramMessage, parseEntities, ParsedMessageEntities } from './Telegram'
+import {
+  TelegramMessage,
+  parseEntities,
+  ParsedMessageEntities,
+} from './Telegram'
 import { Ticktick } from './Ticktick'
 import { Trello } from './Trello'
 
@@ -11,44 +15,77 @@ export type MessageHandlerOptions = {
   ticktickPassword?: string
 }
 
-type CommandHandler = (message: ParsedMessageEntities, options: MessageHandlerOptions) => Promise<{ text: string }>
+type CommandHandler = (
+  message: ParsedMessageEntities,
+  options: MessageHandlerOptions
+) => Promise<{ text: string }>
 
 const commandHandlers: { [key: string]: CommandHandler } = {
-  '/todo': async (message: ParsedMessageEntities, options: MessageHandlerOptions) => {
+  '/todo': async (
+    message: ParsedMessageEntities,
+    options: MessageHandlerOptions
+  ) => {
     if (!options.ticktickEmail) throw new Error('missing ticktickEmail')
     if (!options.ticktickPassword) throw new Error('missing ticktickPassword')
-    const ticktick = new Ticktick(options.ticktickEmail, options.ticktickPassword)
+    const ticktick = new Ticktick(
+      options.ticktickEmail,
+      options.ticktickPassword
+    )
     await ticktick.connect()
-    const desc = `Sent from Telegram-scribe-bot, on ${new Date(message.initial.date * 1000)}`
+    const desc = `Sent from Telegram-scribe-bot, on ${new Date(
+      message.initial.date * 1000
+    )}`
     // note: user's location can be requested, cf https://tutorials.botsfloor.com/request-and-handle-phone-number-and-location-with-telegram-bot-api-e90004c0c87e
     await ticktick.addTask(message.rest, desc)
-    return { text: '✅  Sent to Ticktick\'s inbox' }
+    return { text: "✅  Sent to Ticktick's inbox" }
   },
-  '/today': async (message: ParsedMessageEntities, options: MessageHandlerOptions) => {
+  '/today': async (
+    message: ParsedMessageEntities,
+    options: MessageHandlerOptions
+  ) => {
     if (!options.ticktickEmail) throw new Error('missing ticktickEmail')
     if (!options.ticktickPassword) throw new Error('missing ticktickPassword')
-    const ticktick = new Ticktick(options.ticktickEmail, options.ticktickPassword)
+    const ticktick = new Ticktick(
+      options.ticktickEmail,
+      options.ticktickPassword
+    )
     await ticktick.connect()
-    const desc = `Sent from Telegram-scribe-bot, on ${new Date(message.initial.date * 1000)}`
+    const desc = `Sent from Telegram-scribe-bot, on ${new Date(
+      message.initial.date * 1000
+    )}`
     await ticktick.addTask(message.rest, desc, new Date(), true)
     return { text: '✅  Sent to Ticktick\'s "Today" tasks' }
   },
-  '/note': async (message: ParsedMessageEntities, options: MessageHandlerOptions) => {
+  '/note': async (
+    message: ParsedMessageEntities,
+    options: MessageHandlerOptions
+  ) => {
     if (!options.trelloApiKey) throw new Error('missing trelloApiKey')
     if (!options.trelloUserToken) throw new Error('missing trelloUserToken')
     if (!options.trelloBoardId) throw new Error('missing trelloBoardId')
-    if (!message.tags.length) throw new Error('please specify at least one card as a hashtag')
+    if (!message.tags.length)
+      throw new Error('please specify at least one card as a hashtag')
     const trello = new Trello(options.trelloApiKey, options.trelloUserToken)
     const noteTags = message.tags.map(tagEntity => tagEntity.text)
-    const targetedCards = await trello.getCardsBoundToTags(noteTags, options.trelloBoardId)
-    if (!targetedCards) return { text: `🤔  No cards match these tags. Please retry without another tag.` }
+    const targetedCards = await trello.getCardsBoundToTags(
+      noteTags,
+      options.trelloBoardId
+    )
+    if (!targetedCards)
+      return {
+        text: `🤔  No cards match these tags. Please retry without another tag.`,
+      }
     await Promise.all(
       targetedCards.map(card =>
         trello.card.addComment(card.id, { text: message.rest })
       )
     )
-    return { text: `✅  Sent to Trello cards: ${targetedCards.map(c => c.name).join(', ')}` }
-  }
+    return {
+      text: `✅  Sent to Trello cards: ${targetedCards
+        .map(c => c.name)
+        .join(', ')}`,
+    }
+  },
 }
 
 export async function processMessage(
@@ -68,7 +105,9 @@ export async function processMessage(
     const command = (entities.commands[0] || {}).text
     const commandHandler = commandHandlers[command]
     if (!commandHandler) {
-      text = `🤔  Please retry with a valid command: ${Object.keys(commandHandlers).join(', ')}`
+      text = `🤔  Please retry with a valid command: ${Object.keys(
+        commandHandlers
+      ).join(', ')}`
     } else {
       text = (await commandHandler(entities, options)).text
     }
