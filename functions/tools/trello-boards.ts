77ef1719142bfdcf4875b1182340e48496f9f8de
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2019-12-28 16:00:24 +0100

    fix(ts): convert trello-boards.js to typescript

--- functions/tools/trello-boards.ts
+++ functions/tools/trello-boards.ts
@@ -1,8 +1,11 @@
-require('dotenv').config({ path: `${__dirname}/../../.env` }) // load environment variables
-const { Trello } = require('./../lib/src/Trello')
+import * as dotenv from 'dotenv'
+import { Trello } from './../src/Trello'
+
+dotenv.config({ path: `${__dirname}/../../.env` }) // load environment variables
+
 const { TRELLO_API_KEY, TRELLO_USER_TOKEN } = process.env
 
-new Trello(TRELLO_API_KEY, TRELLO_USER_TOKEN)
+new Trello(TRELLO_API_KEY as string, TRELLO_USER_TOKEN as string)
   .getBoards()
   .then(boards => {
     boards.map(({ id, name }) => console.log(`${id} \t ${name}`))
