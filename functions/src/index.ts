bc151c551b644d06acb3569f75f512da4a5ebe2a
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2019-12-29 09:06:47 +0100

    fix(firebase): "Cannot read property 'config' of undefined"

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,11 @@
 // This file is intended to run the app in production
 
-import functions from 'firebase-functions'
+import * as functions from 'firebase-functions'
 import { makeApp } from './app'
 
-const options = functions.config().config // load credentials and options from firebase config
+const { config } = functions.config() // load credentials and options from firebase config
 
-const app = makeApp(options)
+const app = makeApp(config)
 
 // this is the only function it will be published in firebase
 export const router = functions.https.onRequest(app)
