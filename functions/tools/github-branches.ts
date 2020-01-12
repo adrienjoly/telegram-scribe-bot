ff0648e12924ce3185715476320280d0e7e07dce
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-01-12 14:13:39 +0100

    fetch file from repo's master branch

--- functions/tools/github-branches.ts
+++ functions/tools/github-branches.ts
@@ -1,11 +1,12 @@
 import Octokit from '@octokit/rest'
+import assert from 'assert'
 
 // Load credentials from config file
 const { github } = require(`${__dirname}/../../.config.json`) // eslint-disable-line @typescript-eslint/no-var-requires
 // Note: In order to write to the repo, user must be authenticated with a token
 // that has the "public_repo" permission.
 
-type GitHubRepo = { owner: string; repo: string }
+// type GitHubRepo = { owner: string; repo: string }
 
 const octokit = new Octokit({
   auth: github.token,
@@ -14,6 +15,7 @@ const octokit = new Octokit({
 })
 
 const { owner, repo } = { owner: 'adrienjoly', repo: 'album-shelf' }
+/*
 
 async function getLastCommit(ghRepo: GitHubRepo) {
   return (await octokit.repos.listCommits(ghRepo)).data[0]
@@ -24,7 +26,7 @@ async function createBranch({
   repo,
   name,
 }: GitHubRepo & {
-  name?: string
+  name: string
 }) {
   const { sha } = await getLastCommit({ owner, repo })
   return (
@@ -37,13 +39,53 @@ async function createBranch({
   ).data
 }
 
+async function createCommit({
+  owner,
+  repo,
+  name,
+  message,
+  tree,
+  parents
+}: GitHubRepo & {
+  name?: string
+  message:string
+  tree:
+  parents:
+}) { 
+  octokit.git.createCommit({
+    owner,
+    repo,
+    message,
+    tree,
+    parents
+  })
+}
+*/
+
 async function main() {
+  /*
   const {
     ref,
     node_id,
     object: { sha },
   } = await createBranch({ owner, repo, name: `scribe-bot-test` })
   console.log('=>', { ref, node_id, sha })
+  */
+
+  type ExtendedContent = Octokit.ReposGetContentsResponseItem & {
+    encoding: string
+    content: string
+  }
+  const res = await octokit.repos.getContents({
+    owner,
+    repo,
+    path: '_data/albums.yaml',
+  })
+  const data = res.data as ExtendedContent
+  assert.equal(data.encoding, 'base64')
+  const fileContent = Buffer.from(data.content, 'base64').toString()
+  console.log('=> data:', fileContent)
+  console.log('=>', { sha: data.sha })
 }
 
 main().catch(err => {
