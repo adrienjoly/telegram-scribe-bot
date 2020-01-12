abe72a4774e0f49b5120cc74cf8ba311d0e5b756
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-01-12 16:43:05 +0100

    feat: add GitHub wrapper to submit pull requests (#9)
    `npm run github:test` submits a Pull Request that adds content to a file from https://github.com/adrienjoly/album-shelf.
    
    E.g. https://github.com/adrienjoly/album-shelf/pull/10
    
    The underlying GitHub wrapper is ready to be used from the bot.

---
+++ functions/tools/github-branches.ts
@@ -0,0 +1,33 @@
+import { GitHub } from '../src/GitHub'
+
+// Load credentials from config file
+const { token } = require(`${__dirname}/../../.config.json`).github // eslint-disable-line @typescript-eslint/no-var-requires
+// Note: In order to write to the repo, user must be authenticated with a token
+// that has the "public_repo" permission.
+
+const PULL_REQUEST_DATA = {
+  owner: 'adrienjoly',
+  repo: 'album-shelf',
+  filePath: '_data/albums.yaml',
+  contentToAdd: '\ntest\n',
+}
+
+async function main() {
+  const github = new GitHub({ token })
+  const pr = await github.proposeFileChangePR({
+    ...PULL_REQUEST_DATA,
+    branchName: `scribe-bot-${Date.now()}`,
+    prTitle: `add test to ${PULL_REQUEST_DATA.filePath}`,
+    prBody: 'Submitted by `telegram-scribe-bot`',
+    log: console.warn,
+  })
+  console.warn(`✅ GitHub PR URL: ${pr.url}`)
+}
+
+main().catch(err => {
+  console.error(
+    `❌  ${err.message} <-- ${err.request?.url}\n   ${err.request?.body}`
+  )
+  console.error(err.stack)
+  process.exit(1)
+})
