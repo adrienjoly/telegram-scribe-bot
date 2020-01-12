2bc702d4f703662720cdf5f86a63ee268cf62500
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-01-12 17:30:03 +0100

    format album metadata as YAML

--- functions/tools/spotify-album.ts
+++ functions/tools/spotify-album.ts
@@ -1,7 +1,18 @@
 import SpotifyWebApi from 'spotify-web-api-node'
+import yaml from 'js-yaml'
 
 // Load credentials from config file
 const { clientid, secret } = require(`${__dirname}/../../.config.json`).spotify // eslint-disable-line @typescript-eslint/no-var-requires
+
+const formatAlbum = (album: SpotifyApi.AlbumObjectSimplified) => ({
+  title: album.name,
+  artist: /*album.artistName ||*/ album.artists
+    .map(artist => artist.name)
+    .join(', '),
+  release_date: album.release_date,
+  img: album.images[0].url,
+  url: `https://open.spotify.com/album/${album.id}`,
+})
 
 async function main() {
   // Create the api object with the credentials
@@ -17,8 +28,9 @@ async function main() {
   await spotifyApi.setAccessToken(auth.access_token)
 
   // Get multiple albums
-  const { body: album } = await spotifyApi.getAlbums(['62KA8cUOqlIg1gxbVqBieD'])
-  console.log('Albums information', album)
+  const { body: album } = await spotifyApi.getAlbum('62KA8cUOqlIg1gxbVqBieD')
+  console.log('Albums information', formatAlbum(album))
+  console.log('=> YAML:', yaml.dump([formatAlbum(album)]))
 
   console.warn(`✅ Done.`)
 }
