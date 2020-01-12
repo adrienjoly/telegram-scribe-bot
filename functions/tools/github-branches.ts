ad8a5692420b8e4111c5fe930a1f44721b5968c4
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-01-12 13:42:09 +0100

    create a branch

--- functions/tools/github-branches.ts
+++ functions/tools/github-branches.ts
@@ -1,7 +1,11 @@
 import Octokit from '@octokit/rest'
 
-// load credentials from config file
+// Load credentials from config file
 const { github } = require(`${__dirname}/../../.config.json`) // eslint-disable-line @typescript-eslint/no-var-requires
+// Note: In order to write to the repo, user must be authenticated with a token
+// that has the "public_repo" permission.
+
+type GitHubRepo = { owner: string; repo: string }
 
 const octokit = new Octokit({
   auth: github.token,
@@ -9,38 +13,43 @@ const octokit = new Octokit({
   // log: console, // uncomment this line to trace debug info
 })
 
-const { owner, repo } = { owner: 'adrienjoly2', repo: 'album-shelf' }
+const { owner, repo } = { owner: 'adrienjoly', repo: 'album-shelf' }
 
-async function getLastCommit({ owner, repo }: { owner: string; repo: string }) {
-  const { data } = await octokit.repos
-    //.listBranches()
-    .listCommits({
-      owner,
-      repo,
-    })
-  return data[0]
+async function getLastCommit(ghRepo: GitHubRepo) {
+  return (await octokit.repos.listCommits(ghRepo)).data[0]
 }
 
-async function createBranch({ owner, repo }: { owner: string; repo: string }) {
+async function createBranch({
+  owner,
+  repo,
+  name = `scribe-bot-test`,
+}: GitHubRepo & {
+  name?: string
+}) {
   const { sha, commit } = await getLastCommit({ owner, repo })
   console.log(`last commit: (${sha}) ${commit.message}`)
   const { data } = await octokit.git.createRef({
     owner,
     repo,
-    ref: 'refs/heads/invalid branch name XXX',
+    ref: `refs/heads/${name}`,
     sha,
   })
   return data
 }
 
 async function main() {
-  const data = await createBranch({ owner, repo })
-  console.log('=>', data)
+  const {
+    ref,
+    node_id,
+    object: { sha },
+  } = await createBranch({ owner, repo })
+  console.log('=>', { ref, node_id, sha })
 }
 
 main().catch(err => {
   console.error(
     `❌  ${err.message} <-- ${err.request?.url}\n   ${err.request?.body}`
   )
+  console.error(err.stack)
   process.exit(1)
 })
