f6603ad95e099740b1a94653ace0b09158f1a034
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-01-12 12:59:14 +0100

    use github token from config.json file + reduce log output

--- functions/tools/github-branches.ts
+++ functions/tools/github-branches.ts
@@ -1,17 +1,22 @@
 import Octokit from '@octokit/rest'
 
 // load credentials from config file
-// const { github } = require(`${__dirname}/../../.config.json`) // eslint-disable-line @typescript-eslint/no-var-requires
+const { github } = require(`${__dirname}/../../.config.json`) // eslint-disable-line @typescript-eslint/no-var-requires
 
 const octokit = new Octokit({
+  auth: github.token,
   userAgent: 'telegram-scribe-bot',
-  log: console,
+  // log: console, // uncomment this line to trace debug info
 })
 
 octokit.repos
   .listBranches({ owner: 'adrienjoly', repo: 'album-shelf' })
-  .then(({ data, headers, status }) => {
-    console.log({ data, headers, status })
+  .then(({ data, status }) => {
+    if (status !== 200) {
+      console.error({ data, status })
+    } else {
+      console.log({ data, status })
+    }
   })
   .catch(err => {
     console.error(err)
