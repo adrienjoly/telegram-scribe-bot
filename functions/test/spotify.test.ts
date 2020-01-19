58ec9a5a25d5a24cc2a563c2c7205f2c56fa29cb
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-01-19 19:18:53 +0100

    add (almost-)end-to-end test

--- functions/test/spotify.test.ts
+++ functions/test/spotify.test.ts
@@ -38,8 +38,8 @@ describe('spotify use cases', () => {
   })
   describe('addSpotifyAlbumToShelfRepo', () => {
     before(() => {
-      nock.emitter.on('no match', ({ method, path }) =>
-        console.warn(`⚠ no match for ${method} ${path}`)
+      nock.emitter.on('no match', ({ method, host, path }) =>
+        console.warn(`⚠ no match for ${method} ${host}${path}`)
       )
     })
 
@@ -61,7 +61,47 @@ describe('spotify use cases', () => {
 
     it('extracts the albumId and generates a pull request', async () => {
       const message = createMessage({ rest: SAMPLE_URL })
-      await addSpotifyAlbumToShelfRepo(message, FAKE_CREDS)
+      nock('https://accounts.spotify.com')
+        .post(uri => uri.includes(`/api/token`))
+        .reply(200, { token: 'token' })
+      nock('https://api.spotify.com')
+        .get(uri => uri.includes(`/v1/albums/${SAMPLE_ALBUM_ID}`))
+        .reply(200, {
+          title: 'coucou',
+          artists: [{ name: 'artist' }],
+          images: [{ url: '' }],
+        })
+      nock('https://api.github.com')
+        .get(uri => uri.includes(`/repos/adrienjoly/album-shelf/commits`)) // TODO: make the repo customizable
+        .reply(200, [{ sha: 'sha', commit: { tree: { sha: 'treesha' } } }])
+      nock('https://api.github.com')
+        .get(uri =>
+          uri.includes(
+            `/repos/adrienjoly/album-shelf/contents/_data/albums.yaml`
+          )
+        )
+        .reply(200, {
+          encoding: 'base64',
+          content: 'ABAB',
+          sha: 'filesha',
+        })
+      nock('https://api.github.com')
+        .post(uri => uri.includes('/repos/adrienjoly/album-shelf/git/blobs'))
+        .reply(200, { sha: 'blobsha' })
+      nock('https://api.github.com')
+        .post(uri => uri.includes('/repos/adrienjoly/album-shelf/git/trees'))
+        .reply(200, { sha: 'newtreesha' })
+      nock('https://api.github.com')
+        .post(uri => uri.includes('/repos/adrienjoly/album-shelf/git/commits'))
+        .reply(200, { sha: 'commitsha' })
+      nock('https://api.github.com')
+        .post('/repos/adrienjoly/album-shelf/git/refs')
+        .reply(200, {})
+      nock('https://api.github.com')
+        .post('/repos/adrienjoly/album-shelf/pulls')
+        .reply(200, { url: '//successful-pr' })
+      const res = await addSpotifyAlbumToShelfRepo(message, FAKE_CREDS)
+      expect(res).toHaveProperty('text', '✅  Submitted PR on //successful-pr')
     })
   })
 })
