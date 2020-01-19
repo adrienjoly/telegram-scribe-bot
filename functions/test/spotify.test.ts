a63d686d7119812229dac335a84213bd701aac90
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-01-19 18:41:49 +0100

    refact: extract constants

--- functions/test/spotify.test.ts
+++ functions/test/spotify.test.ts
@@ -3,12 +3,13 @@
 import expect from 'expect'
 import { parseAlbumId } from './../src/use-cases/addSpotifyAlbumToShelfRepo'
 
+const SAMPLE_ALBUM_ID = '62KA8cUOqlIg1gxbVqBieD'
+const SAMPLE_URL = `https://open.spotify.com/album/${SAMPLE_ALBUM_ID}?si=aggz_NaqSle0D0rsRLpGgA`
+
 describe('spotify use cases', () => {
   describe('parseAlbumId', () => {
     it('detects album id from URL', async () => {
-      expect(
-        parseAlbumId(
-          'https://open.spotify.com/album/62KA8cUOqlIg1gxbVqBieD?si=aggz_NaqSle0D0rsRLpGgA'
+      expect(parseAlbumId(SAMPLE_URL)).toEqual(SAMPLE_ALBUM_ID)
         )
       ).toEqual('62KA8cUOqlIg1gxbVqBieD')
     })
