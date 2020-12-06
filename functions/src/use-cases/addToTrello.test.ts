c283b4f1e9aff252136f611f208a6a3476f11000
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-12-06 18:50:57 +0100

    reduce redundant errors logging in tests

--- functions/src/use-cases/addToTrello.test.ts
+++ functions/src/use-cases/addToTrello.test.ts
@@ -268,7 +268,6 @@ describe('trello use cases', () => {
       })
       const res = await getNextTrelloTasks(message, FAKE_CREDS)
       // check expectation
-      if (res.error) console.error(res.error)
       expect(res.text).toEqual(expectedResult)
     })
 
