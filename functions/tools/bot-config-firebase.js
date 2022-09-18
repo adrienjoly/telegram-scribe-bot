adrienjoly/telegram-scribe-bot

commit 0db2583105e737c4e9a0dc0025453ee65ce4e148

fix(firebase): `Invalid argument, each config value must have a 2-part key (e.g. foo.bar).` cf https://github.com/firebase/firebase-tools/issues/406#issuecomment-853992173

functions/tools/bot-config-firebase.js
@@ -3,7 +3,15 @@
 
 const { execSync } = require('child_process') // eslint-disable-line @typescript-eslint/no-var-requires
 const botConfig = require(`${__dirname}/bot-config.js`) // eslint-disable-line @typescript-eslint/no-var-requires
-const json = JSON.stringify(botConfig, null, 2)
-execSync(`npx firebase functions:config:set config='${json}'`, {
+
+const flattened = Object.entries(botConfig)
+  .map(([key, value]) =>
+    Object.entries(value)
+      .map(([k, v]) => `${key}.${k}=${v}`)
+      .join(' ')
+  )
+  .join(' ')
+
+execSync(`npx firebase functions:config:set ${flattened}`, {
   stdio: 'inherit',
 })

commit 6ccc658561a174456a776bd45846b2c23527a826

fix(config): make sure that bot version is up to date regression came from https://github.com/adrienjoly/telegram-scribe-bot/commit/0db2583105e737c4e9a0dc0025453ee65ce4e148

functions/tools/bot-config-firebase.js
@@ -7,7 +7,7 @@ const botConfig = require(`${__dirname}/bot-config.js`) // eslint-disable-line @
 const flattened = Object.entries(botConfig)
   .map(([key, value]) =>
     Object.entries(value)
-      .map(([k, v]) => `${key}.${k}=${v}`)
+      .map(([k, v]) => `config.${key}.${k}=${v}`)
       .join(' ')
   )
   .join(' ')