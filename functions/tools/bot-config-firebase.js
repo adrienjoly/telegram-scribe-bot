30493b455394d621647c45af7d47edeb4ad8978f
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2025-01-18 13:00:34 +0100

    fix(deploy): prevent part of secret values separated by special characters from causing `command not found`

--- functions/tools/bot-config-firebase.js
+++ functions/tools/bot-config-firebase.js
@@ -4,10 +4,18 @@
 const { execSync } = require('child_process') // eslint-disable-line @typescript-eslint/no-var-requires
 const botConfig = require(`${__dirname}/bot-config.js`) // eslint-disable-line @typescript-eslint/no-var-requires
 
+/**
+ * Escape single quotes and wrap in single quotes,
+ * to prevent special characters from being interpreted by the shell.
+ */
+function escapeShellValue(value) {
+  return `'${String(value).replace(/'/g, "'\\''")}'`
+}
+
 const flattened = Object.entries(botConfig)
   .map(([key, value]) =>
     Object.entries(value)
-      .map(([k, v]) => `config.${key}.${k}=${v}`)
+      .map(([k, v]) => `config.${key}.${k}=${escapeShellValue(v)}`)
       .join(' ')
   )
   .join(' ')
